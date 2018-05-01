b4a5cba68b25068d9e48566f8682c470a24c563c
Shawn Zhang <shawn.zhang@razerzone.com>
2018-05-01 10:59:08 +0800

    Init

---
+++ 1_StaticWebHosting/website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_DyvYtTN1k', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'sd3881ir0qi34lc4touc7utg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
